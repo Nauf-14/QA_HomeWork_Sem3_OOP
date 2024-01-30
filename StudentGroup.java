86ac808c052b0ff9a78ae6a4e22f72d33dd5eab8
Naufal <nauf1207@mail.ru>
2024-01-30 21:38:19 +0600

    StudentGroup

---
+++ StudentGroup.java
@@ -0,0 +1,12 @@
+public class StudentGroup {
+    private String name;
+
+    public StudentGroup(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
